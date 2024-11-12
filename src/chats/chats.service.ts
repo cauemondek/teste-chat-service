c5209a1bec9befb1c3cfc77696b96fed848d5d72
cauemondek <cauemondekk@gmail.com>
2024-11-12 19:10:56 -0300

    :goal_net: errors: Adicionado tratamento de erros para os serviços de Chat

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Chat } from '../model/chat.entity';
 import { Repository } from 'typeorm';
 import { User } from '../model/user.entity';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class ChatsService {
@@ -14,30 +15,61 @@ export class ChatsService {
   ) {}
 
   async create(chat: Chat): Promise<Chat> {
-    return this.chatsRepository.save(chat);
+    const response: Chat = await this.chatsRepository.save(chat).catch(error => {
+      throw new HttpException(`It was not possible to Create a new chat: ${error}`, 500);
+    });
+
+    return response;
   }
 
   async findAll(): Promise<Chat[]> {
-    return this.chatsRepository.find({ relations: ['users'] });
+    const response: Chat[] = await this.chatsRepository.find({ relations: ['users'] });
+
+    if (response.length < 1) {
+      Logger.warn('No stored chat found');
+    }
+
+    return response;
   }
 
   async findOne(id: number): Promise<Chat> {
-    return this.chatsRepository.findOne({ where: { id }, relations: ['users'] });
+    const response: Chat = await this.chatsRepository.findOne({ where: { id }, relations: ['users'] });
+
+    if (!response) {
+      throw new NotFoundException('Chat not found');
+    }
+
+    return response;
   }
 
   async update(id: number, chat: Chat): Promise<Chat> {
-    await this.chatsRepository.update(id, chat);
-    return this.findOne(id);
+    const response: Chat = await this.findOne(id);
+
+    if (!response) {
+      throw new NotFoundException('Chat not found for update');
+    }
+
+    await this.chatsRepository.update(id, chat).catch(error => {
+      throw new HttpException(`Error when Update Chat: ${error}`, 500);
+    });
+
+    return response;
   }
 
   async remove(id: number): Promise<void> {
+    await this.findOne(id).catch(error => {
+      throw new NotFoundException(`Chat not found for remove: ${error}`);
+    });
+
     for await (const user of (await this.chatsRepository.findOne({ where: { id: id }, relations: ['users'] })).users) {
       user.chatsCount--;
 
       await this.userRepository.save(user);
     }
 
-    await this.chatsRepository.delete(id);
+    await this.chatsRepository.delete(id).catch(error => {
+      throw new HttpException(`Error when Remove Chat: ${error}`, 500);
+    });
   }
 
   async addUserToChat(chatId: number, userId: number): Promise<Chat> {
@@ -45,13 +77,19 @@ export class ChatsService {
     const user = await this.userRepository.findOne({ where: { id: userId } });
 
     if (!chat || !user) {
-      throw new Error('Chat or User not found');
+      throw new NotFoundException('Chat or User not found');
     }
 
     chat.users.push(user);
     chat.usersCount++;
     user.chatsCount++;
 
-    return this.chatsRepository.save(chat);
+    const response = await this.chatsRepository.save(chat);
+
+    if (!response) {
+      throw new HttpException('Error when Save the Chat', 500);
+    }
+
+    return response;
   }
 }
