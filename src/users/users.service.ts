3e22500f46ab26584499f1f80f58e5c7627957a9
cauemondek <cauemondekk@gmail.com>
2024-11-12 19:11:18 -0300

    :goal_net: errors: Adicionado tratamento de erros para os serviços de Users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../model/user.entity';
 import { Repository } from 'typeorm';
@@ -14,30 +14,66 @@ export class UsersService {
   ) {}
 
   async create(name: string): Promise<User> {
-    const user = this.userRepository.create({ name });
-    return this.userRepository.save(user);
+    const user: User = this.userRepository.create({ name });
+
+    if (!user) {
+      throw new HttpException('It was not possible to Create a new user', 500);
+    }
+
+    const response: User = await this.userRepository.save(user).catch(error => {
+      throw new HttpException(`It was not possible to Save the new user: ${error}`, 500);
+    });
+
+    return response;
   }
 
   async findAll(): Promise<User[]> {
-    return this.userRepository.find({ relations: ['chats'] });
+    const response: User[] = await this.userRepository.find({ relations: ['chats'] });
+
+    if (response.length < 1) {
+      Logger.warn('No stored user found');
+    }
+
+    return response;
   }
 
   async findOne(id: number): Promise<User> {
-    return this.userRepository.findOne({ where: { id }, relations: ['chats'] });
+    const response: User = await this.userRepository.findOne({ where: { id }, relations: ['chats'] });
+
+    if (!response) {
+      throw new NotFoundException('User not found');
+    }
+
+    return response;
   }
 
   async update(id: number, name: string): Promise<User> {
-    await this.userRepository.update(id, { name });
-    return this.findOne(id);
+    const response: User = await this.findOne(id);
+
+    if (!response) {
+      throw new NotFoundException('User not found for update');
+    }
+
+    await this.userRepository.update(id, { name }).catch(error => {
+      throw new HttpException(`Error when Update User: ${error}`, 500);
+    });
+
+    return response;
   }
 
   async remove(id: number): Promise<void> {
+    await this.findOne(id).catch(error => {
+      throw new NotFoundException(`User not found for remove: ${error}`);
+    });
+
     for await (const chat of (await this.userRepository.findOne({ where: { id: id }, relations: ['chats'] })).chats) {
       chat.usersCount--;
 
       await this.chatsRepository.save(chat);
     }
 
-    await this.userRepository.delete(id);
+    await this.userRepository.delete(id).catch(error => {
+      throw new HttpException(`Error when Remove User: ${error}`, 500);
+    });
   }
 }
